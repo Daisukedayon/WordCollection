27acf87aeb90b2898f214f91d27327b466446059
おかやん <okadaisuke@okayan-no-MacBook-Pro.local>
2014-09-16 02:54:08 +0900

    TextHTMLParser modified

--- OpenMedicalWebSite.py
+++ OpenMedicalWebSite.py
@@ -5,32 +5,12 @@
 import urllib2
 
 #import HTMLParser
-from HTMLParser import HTMLParser, HTMLParseError
+
 
 
 from MedicalWordURLCollection import MedicalWordURLStore
 
-class TestHTMLParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.is_a = False
-    
-    def handle_starttag(self, tag, attrs):
-    	if tag.lower() == "a":
-    		attrs = dict(attrs)
-    		class_val = "crosslink"
-    		if "class" in attrs:
-    			if class_val == attrs["class"]:
-      				self.is_a = True
 
-
-    def handle_endtag(self, tag):
-    	if tag.lower() == "a":
-    		self.is_a = False
-
-    def handle_data(self, data):
-    	if self.is_a is True:
-        	print(data)
 
 
 URLCollection = ['http://ejje.weblio.jp/category/healthcare/eigky/aa']
---
+++ TextHTMLParser.py
@@ -0,0 +1,26 @@
+from HTMLParser import HTMLParser, HTMLParseError
+
+
+class TestHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.is_a = False
+        self.WordArray = []
+    
+    def handle_starttag(self, tag, attrs):
+    	if tag.lower() == "a":
+    		attrs = dict(attrs)
+    		class_val = "crosslink"
+    		if "class" in attrs:
+    			if class_val == attrs["class"]:
+      				self.is_a = True
+
+
+    def handle_endtag(self, tag):
+    	if tag.lower() == "a":
+    		self.is_a = False
+
+    def handle_data(self, data):
+    	if self.is_a is True:
+            self.WordArray.append(data)
+        	#print(data)
