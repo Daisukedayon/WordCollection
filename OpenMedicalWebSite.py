60aabe6b3fd73b6652ffe05d1434bf95e6a1728b
おかやん <okadaisuke@okayan-no-MacBook-Pro.local>
2014-09-15 20:25:27 +0900

    最初のコミット

---
+++ OpenMedicalWebSite.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#医療単語を追加する
+
+
+import urllib2
+
+#import HTMLParser
+from HTMLParser import HTMLParser, HTMLParseError
+
+class TestHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.is_a = False
+    
+    def handle_starttag(self, tag, attrs):
+    	if tag.lower() == "a":
+    		attrs = dict(attrs)
+    		class_val = "crosslink"
+    		if "class" in attrs:
+    			if class_val == attrs["class"]:
+    			
+    				self.is_a = True
+        # attrs = dict(attrs) # タプルだと扱いにくいので辞書にする
+        # print 'start', tag
+        # if 'div' == tag and 'class' in attrs:
+        #      print '-->', attrs['class']
+
+    def handle_endtag(self, tag):
+    	if tag.lower() == "a":
+    		self.is_a = False
+
+    def handle_data(self, data):
+    	if self.is_a is True:
+        	print(data)
+
+
+
+
+
+
+
+response = urllib2.urlopen('http://ejje.weblio.jp/category/healthcare/eigky/aa')
+html = response.read()
+
+parser = TestHTMLParser()
+parser.feed(html)
+parser.close()
+
